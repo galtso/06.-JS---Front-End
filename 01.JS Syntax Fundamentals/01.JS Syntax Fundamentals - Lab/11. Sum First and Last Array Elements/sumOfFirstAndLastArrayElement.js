8b3bff02431d12119d2d1ba3360000f87ad1348a
galtso <galcho_ta@yahoo.com>
2023-02-25 19:35:34 +0200

    sumOfFirstAndLastArrayElement.js

---
+++ 01.JS Syntax Fundamentals/01.JS Syntax Fundamentals - Lab/11. Sum First and Last Array Elements/sumOfFirstAndLastArrayElement.js
@@ -0,0 +1,7 @@
+function sumOfElements(input) {
+    let firstElement = input[0];
+    let lastElement = input[input.length - 1];
+    console.log(firstElement + lastElement);
+}
+
+sumOfElements([20, 30, 40])
