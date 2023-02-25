2c124e3b4344cbf8ff6f2e8037d49c4d29177639
galtso <galcho_ta@yahoo.com>
2023-02-25 15:40:48 +0200

    02.studentInfo.js

---
+++ 01.JS Syntax Fundamentals/01.JS Syntax Fundamentals - Lab/02.Student Information/studentInfo.js
@@ -0,0 +1,7 @@
+function printStudentInfo(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+printStudentInfo('Steve', 16, 2.1426)
+printStudentInfo('John', 15, 5.54678)
+printStudentInfo('Marry', 12, 6.00)
