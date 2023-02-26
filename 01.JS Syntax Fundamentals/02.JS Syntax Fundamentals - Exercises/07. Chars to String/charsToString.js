cccbadf83d31fd6e2877064f5cb719394a64231e
galtso <galcho_ta@yahoo.com>
2023-02-26 22:38:48 +0200

    charsToString.js

---
+++ 01.JS Syntax Fundamentals/02.JS Syntax Fundamentals - Exercises/07. Chars to String/charsToString.js
@@ -0,0 +1,16 @@
+function solve(char1, char2, char3) {
+    let string = char1 + char2 + char3;
+    console.log(string);
+}
+solve('a',
+'b',
+'c'
+);
+solve('%',
+'2',
+'o'
+);
+solve('1',
+'5',
+'p'
+);
