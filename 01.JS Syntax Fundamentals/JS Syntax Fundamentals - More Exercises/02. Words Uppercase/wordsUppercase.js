436e5845832b824361a6068cb7cec1e76b061e34
galtso <galcho_ta@yahoo.com>
2023-03-03 02:20:29 +0200

    wordsUppercase.js

--- 01.JS Syntax Fundamentals/JS Syntax Fundamentals - More Exercises/02. Words Uppercase/wordsUppercase.js
+++ 01.JS Syntax Fundamentals/JS Syntax Fundamentals - More Exercises/02. Words Uppercase/wordsUppercase.js
@@ -1,21 +1,27 @@
-function solve(line) {
-    let firstArray = line.split(" ");
-    let result = [];
-    for (const word of firstArray) {
-        let currentWord = "";
-        for (const letter of word) {
-            if (letter.toUpperCase() !== letter.toLowerCase()) {
-                currentWord += letter.toUpperCase();
-            }else{
-                result.push(currentWord)
-                currentWord = "";
-            }
-        }
-        if (currentWord) {
-            result.push(currentWord);
-        }
-    }
-    console.log(result.join(", "));
+// function solve(line) {
+//     let firstArray = line.split(" ");
+//     let result = [];
+//     for (const word of firstArray) {
+//         let currentWord = "";
+//         for (const letter of word) {
+//             if (letter.toUpperCase() !== letter.toLowerCase()) {
+//                 currentWord += letter.toUpperCase();
+//             }else{
+//                 result.push(currentWord)
+//                 currentWord = "";
+//             }
+//         }
+//         if (currentWord) {
+//             result.push(currentWord);
+//         }
+//     }
+//     console.log(result.join(", "));
+// }
+
+function solve(string) {
+    const words = string.match(/\b\w+\b/g) || [];
+    const upperCaseWords = words.map(word => word.toUpperCase());
+    console.log(upperCaseWords.join(', '));
 }
 
 solve('Hi, how are you?');
