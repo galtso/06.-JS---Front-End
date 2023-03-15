2eb32996636da616d97f8cf0b1530784fb80dbf1
galtso <galcho_ta@yahoo.com>
2023-03-15 16:08:50 +0200

    employees.js

---
+++ 03. Objects and Classes/02.Objects and Classes - Exercises/01. Employees/employees.js
@@ -0,0 +1,18 @@
+function solve(input){
+let employees = {};
+for (const line of input) {
+    employees[line] = line.length;
+}
+
+for (const key in employees) {
+    console.log(`Name: ${key} -- Personal Number: ${employees[key]}`)
+}
+}
+
+solve([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    )
