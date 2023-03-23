6d32bfba74e61e457892d8048851a5b194db289e
galtso <galcho_ta@yahoo.com>
2023-03-23 17:08:15 +0200

    script.js

---
+++ 04. DOM and Events/02. DOM and Events - Exercises/02. Sections/script.js
@@ -0,0 +1,14 @@
+function create(words) {
+   const content = document.getElementById('content');
+   console.log(content)
+   for (const word of words) {
+      const newDiv = document.createElement('div');
+      const newParagraph = document.createElement('p');
+      newParagraph.textContent = word;
+      newParagraph.style.display = 'none';
+      newDiv.addEventListener('click', () => {newParagraph.style.display = 'block'});
+
+      newDiv.appendChild(newParagraph);
+      content.appendChild(newDiv);
+   }
+}
